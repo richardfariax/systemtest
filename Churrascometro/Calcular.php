e00fbd6098880b4158dec37ef4819daf8accb1fe
richard <richard@highsoftsistemas.com.br>
2023-05-02 16:11:02 -0300

    Melhorias de validações no churrascômetro; Mudanças no titulo da pagina;

--- Churrascometro/Calcular.php
+++ Churrascometro/Calcular.php
@@ -5,6 +5,14 @@ $cerveja = $_POST['cerveja'];
 $refrigerante = $_POST['refrigerante'];
 $suco = $_POST['suco'];
 
+function linguica($adultos, $criancas)
+{
+    $carne1 = $adultos * 0.150;
+    $carne2 = $criancas * 0.050;
+
+    return $carne1 + $carne2;
+}
+
 function carne($adultos, $criancas)
 {
     $carne1 = $adultos * 0.400;
@@ -15,7 +23,7 @@ function carne($adultos, $criancas)
 
 function bebidas($cerveja, $refrigerante, $suco)
 {
-    $latas = $cerveja * 4;
+    $latas = $cerveja * 5;
     $refrigerantes = $refrigerante * 1;
     $sucos = $suco * 0.700;
 
@@ -28,9 +36,11 @@ if ($adultos) {
     list($total_cervejas, $total_refrigerantes, $total_sucos) = bebidas($cerveja, $refrigerante, $suco);
     echo "<p style='margin-bottom: 5px;'>Total de latas de cerveja: $total_cervejas</p>";
     echo "<p style='margin-bottom: 5px;'>Total de refrigerantes: $total_refrigerantes Litros</p>";
-    echo "<p style='margin-bottom: 5px;'>Total de suco em litros: $total_sucos Litros</p>";
+    echo "<p style='margin-bottom: 5px;'>Total de suco: $total_sucos Litros</p>";
     $total_carne = carne($adultos, $criancas);
+    $total_linguica = linguica($adultos, $criancas);
     echo "<p style='margin-bottom: 5px;'>Total de Carne: $total_carne Kg</p>";
+    echo "<p style='margin-bottom: 5px;'>Total de Linguiça: $total_linguica Kg</p>";
     echo "</div>";
 }
 ?>
